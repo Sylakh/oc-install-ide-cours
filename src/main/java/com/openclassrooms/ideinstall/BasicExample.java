984470c791680adf9a1e7452746da658fb01b51b
Sylakh <sylvainsds@gmail.com>
2023-08-11 00:27:17 +0200

    dummy comment 2

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,7 +1,7 @@
 package com.openclassrooms.ideinstall;
 
 public class BasicExample {
-// dummy comment
+// dummy comment 2
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 3; i++) {
